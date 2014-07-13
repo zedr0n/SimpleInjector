f82a43fac08a2bb3c54d88f2f974594ce7a06a97
.NET Junkie <steven@cuttingedge.it>
2014-07-13 16:23:03 +0200

    Prevented dynamic assembly compilation when the debugger is attached, to prevent the "JIT Compiler encountered an internal limitation" exception that some users are experiencing in VS2013 (See work item 20904).

--- SimpleServiceLocator/SimpleInjector.NET/CompilationHelpers.cs
+++ SimpleServiceLocator/SimpleInjector.NET/CompilationHelpers.cs
@@ -24,6 +24,7 @@ namespace SimpleInjector
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
@@ -56,7 +57,12 @@ namespace SimpleInjector
             // unloaded). We might however relax this constraint and optimize the first N container instances.
             if (container.Options.EnableDynamicAssemblyCompilation)
             {
-                TryCompileInDynamicAssembly<TResult>(expression, ref compiledLambda);
+                // HACK: Prevent "JIT Compiler encountered an internal limitation" exception while running in 
+                // the debugger with VS2013 (See work item 20904).
+                if (!Debugger.IsAttached)
+                {
+                    TryCompileInDynamicAssembly<TResult>(expression, ref compiledLambda);
+                }
             }
 
             return compiledLambda ?? CompileLambda<TResult>(expression);
